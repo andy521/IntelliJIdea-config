c0fa0b66f797d12ec3a079e82eb20fd93b462854
Jerry.Chen <ayao1010@gmail.com>
2015-10-23 14:22:49 +0800

    add ZAJavaFileHeader

---
+++ fileTemplates/includes/ZAJavaFileHeader.java
@@ -0,0 +1,10 @@
+#set($str = "")
+#set($stringClass = $str.getClass())
+#set($date = $stringClass.forName("java.util.Date").newInstance())
+#set($dateFormat = $stringClass.forName("java.text.SimpleDateFormat").getConstructor($stringClass).newInstance("yyyy年MM月dd日 ah:mm:ss"))
+#set($fdate = $dateFormat.format($date))
+/**
+ * 类${NAME}的实现描述：TODO 类实现描述
+ * 
+ * @author ${USER} $fdate
+ */
