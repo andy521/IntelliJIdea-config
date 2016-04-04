4863a74f15c05ca73d4e85627dc92a596d1fa522
Jerry.Chen <jerry.myoss@gmail.com>
2016-04-04 14:02:31 +0800

    java file templates 中的日期格式修改为：yyyy年M月d日 ah:mm:ss

--- fileTemplates/includes/SvnJavaFileHeader.java
+++ fileTemplates/includes/SvnJavaFileHeader.java
@@ -1,7 +1,7 @@
 #set($str = "")
 #set($stringClass = $str.getClass())
 #set($date = $stringClass.forName("java.util.Date").newInstance())
-#set($dateFormat = $stringClass.forName("java.text.SimpleDateFormat").getConstructor($stringClass).newInstance("yyyy年MM月dd日 ah:mm:ss"))
+#set($dateFormat = $stringClass.forName("java.text.SimpleDateFormat").getConstructor($stringClass).newInstance("yyyy年M月d日 ah:mm:ss"))
 #set($fdate = $dateFormat.format($date))
 /**
  * Created by Yao.Chen on $fdate.
